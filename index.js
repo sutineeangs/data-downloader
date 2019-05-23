0ef5623c1b171abc7f974f35e056426de5c290d3
sutinee742 <sutinee742@gmail.com>
2019-05-23 20:55:33 +0700

    export downloader

--- index.js
+++ index.js
@@ -3,32 +3,7 @@ const args = require('minimist')(process.argv.slice(2));
 const _ = require('underscore');
 const fs = require('fs');
 const getUri = require('get-uri');
-
-
-
-// function download(uri: String!, options: Object!)
-/* Any other options passed in to the options object will be passed through to 
-   the low-level connection creation functions (http.get(), ftp.connect(), etc). */
-const download = (uri, options, callback) => {
-    getUri(uri, options, (err, res) => {
-        if (err) return callback(err);
-
-        // create directory
-        let directory = `./download`
-        let filename = `${_.last(uri.split("/"))}`
-
-        fs.mkdir(directory, { recursive: true }, (err) => {
-            if (err) return callback(err);
-
-            //write file to local
-            let writeStream = fs.createWriteStream(`${directory}/${filename}`, { flags: 'w' });
-            res.pipe(writeStream);
-            writeStream.on('close', function () {
-                callback(null, true)
-            });
-        });
-    })
-}
+const download = require('./downloader');
 
 
 const main = () => {
